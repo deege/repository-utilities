c86f212cbd078b27e814984e1df9f7d1121fa573
DJ Spiess <dspiess@nso.edu>
2016-09-09 14:05:01 -0600

    Fixed identifier for properties file

--- src/main/java/com/deegeu/utilities/git/GitRepositoryState.java
+++ src/main/java/com/deegeu/utilities/git/GitRepositoryState.java
@@ -36,8 +36,8 @@ public class GitRepositoryState {
         this.branch = String.valueOf(properties.get("git.branch"));
         this.dirty = String.valueOf(properties.get("git.dirty"));
         this.remoteOriginUrl = String.valueOf(properties.get("git.remote.origin.url"));
-
-        this.commitId = String.valueOf(properties.get("git.commit.id.full")); // OR properties.get("git.commit.id") depending on your configuration
+        
+        this.commitId = String.valueOf(properties.get("git.commit.id")); 
         this.commitIdAbbrev = String.valueOf(properties.get("git.commit.id.abbrev"));
         this.describe = String.valueOf(properties.get("git.commit.id.describe"));
         this.describeShort = String.valueOf(properties.get("git.commit.id.describe-short"));
